872ea83038a36d8a7dd461bfa8c437bd93b5baf8
avinashdane1991 <avinash.dane@lenze.com>
2022-01-18 15:08:17 +0530

    adding build folder

---
+++ build/CMakeFiles/profilegenapp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/profilegenapp.dir/src/app.cpp.o"
+  "CMakeFiles/profilegenapp.dir/src/app.cpp.o.d"
+  "lib/libprofilegenapp.pdb"
+  "lib/libprofilegenapp.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/profilegenapp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
